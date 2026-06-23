e28d71be2f40fa29b9ac900caa41e1a2874b190c
Luiz Felipe Frez <luiz.frez.santos@escola.pr.gov.br>
2026-06-23 23:56:45 +0000

    ok01

--- Exercicio6.java
+++ Exercicio6.java
@@ -7,15 +7,17 @@ public class Exercicio6 {
         System.out.println("Digite o valor da compra");
         double valorCompra = scanner.nextDouble();
 
-        if (valorCompra <= 200) {
-            Double valorDescontado = valorCompra * 0.8;
-            System.out.println("O valor da sua compra ficou R$" + valorDescontado);
+        if (valorCompra >= 100 && valorCompra <=200) {
+            Double valorDescontado = valorCompra * 0.10;
+            System.out.println("O valor de seu desconto ficou R$" + valorDescontado);
 
             
         }else if(valorCompra > 200 ){
             Double valorDescontado = valorCompra * 0.20;
-            System.out.println("O valor da sua compra ficou R$" + valorDescontado);
+            System.out.println("O valor de seu desconto ficou R$" + valorDescontado);
 
+        }else {
+            System.out.println("Você não tem desconto!");
         }
     }
 
